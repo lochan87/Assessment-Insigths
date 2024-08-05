3376af03f68a03aa79de6d16a37db534a53c3620
lochan87 <1DS22IS073@dsce.edu.in>
2024-08-05 18:48:05 +0530

    db

--- config/db.js
+++ config/db.js
@@ -1,24 +1,50 @@
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// require('dotenv').config();
+
+// const mongoURI = process.env.MONGO_URI;
+
+// const client = new MongoClient(mongoURI, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     },
+// });
+
+// const connectDB = async () => {
+//     try {
+//         await client.connect();
+//         console.log("Successfully connected to MongoDB!");
+//     } catch (error) {
+//         console.error(error);
+//         process.exit(1);
+//     }
+// };
+
+// module.exports = { connectDB, client };
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
-const mongoURI = process.env.MONGO_URI;
-
-const client = new MongoClient(mongoURI, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-});
+let db;
 
 const connectDB = async () => {
-    try {
-        await client.connect();
-        console.log("Successfully connected to MongoDB!");
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
+    if (db) return db; // Return the existing connection if already connected
+
+    const client = new MongoClient(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    await client.connect();
+    db = client.db('AssessmentInsights');
+    console.log('Successfully connected to MongoDB!');
+    return db;
 };
 
-module.exports = { connectDB, client };
+const getDB = () => {
+    if (!db) throw new Error('Database not connected!');
+    return db;
+};
+
+module.exports = { connectDB, getDB };
--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,16 +1,35 @@
-// controllers/dataController.js
+// // controllers/dataController.js
+// const { getUSN } = require('../store/store');
+// const { client } = require('../config/db');
+// const db = client.db("AssessmentInsights");
+
+// const getData = async (collectionName, res) => {
+//     try {
+//         const usn = getUSN();  // Get the updated USN
+//         const data = await db.collection(collectionName).findOne({ USN: usn });
+//         res.status(200).send(data);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// };
+
+// module.exports = { getData };
+
+const { getDB } = require('../config/db');
 const { getUSN } = require('../store/store');
-const { client } = require('../config/db');
-const db = client.db("AssessmentInsights");
 
 const getData = async (collectionName, res) => {
+    const db = getDB();
+    const usn = getUSN();
+    const projection = { _id: 0 }; // Specify the fields you need
+
     try {
-        const usn = getUSN();  // Get the updated USN
-        const data = await db.collection(collectionName).findOne({ USN: usn });
-        res.status(200).send(data);
+        const data = await db.collection(collectionName).findOne({ USN: usn }, { projection });
+        res.json(data);
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).json({ error: error.message });
     }
 };
 
 module.exports = { getData };
+
